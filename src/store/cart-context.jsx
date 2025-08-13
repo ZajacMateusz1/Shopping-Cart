f0131720e81610335f3fdf3e65f87380f2093d83
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-08-13 21:03:12 +0200

    Add contet in CardModal, add useReducer hook in cart-context, add logo file

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -1,12 +1,27 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 export const CartContext = createContext({
   items: [],
 });
 
+function cartReducer(state, action) {
+  switch (action.type) {
+    case "add_item":
+      break;
+  }
+}
 export default function CartContextProvider({ children }) {
+  const [cartState, cartDispatch] = useReducer(cartReducer, { items: [] });
+  function handleAddItem(id) {
+    cartDispatch({
+      type: "ADD_ITEM",
+      payload: id,
+    });
+  }
+  const cartCTX = {
+    items: cartState.items,
+    addItem: handleAddItem,
+  };
   return (
-    <CartContext.Provider value={{ items: [] }}>
-      {children}
-    </CartContext.Provider>
+    <CartContext.Provider value={cartCTX}>{children}</CartContext.Provider>
   );
 }
