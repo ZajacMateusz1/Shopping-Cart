f396532042ec9768e3f3e8dde33e54c6dcbeef91
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-08-13 21:03:12 +0200

    Add logo file

--- src/components/CardModal/CardModal.jsx
+++ src/components/CardModal/CardModal.jsx
@@ -1,1 +1,21 @@
-export default function CardModal() {}
+import { useContext } from "react";
+import { CartContext } from "../../store/cart-context.jsx";
+import Button from "../Button/Button.jsx";
+export default function CardModal() {
+  const { items } = useContext(CartContext);
+  return (
+    <div role="dialog">
+      <h2>Your Cart</h2>
+      {items.length > 0 ? (
+        <ul>
+          {items.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Your cart is empty</p>
+      )}
+      <Button>Checkout</Button>
+    </div>
+  );
+}
